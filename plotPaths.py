250b1fe69021aca919084578863a81f642d5773e
Aaron Geller <a-geller@northwestern.edu>
2020-02-11 12:16:32 -0600

    changing strategy

--- plotPaths.py
+++ plotPaths.py
@@ -1,3 +1,5 @@
+#! /Users/ageller/anaconda3/bin/python
+
 #why am I getting so many seg faults??
 
 import numpy as np
@@ -7,18 +9,82 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import matplotlib
 import os
+import sys
+
+import argparse
 
 #also see this for plotting specific city:  
 # https://stackoverflow.com/questions/41527234/trying-to-draw-map-of-a-particular-city-with-cartopy
 
-cmap = matplotlib.cm.get_cmap('viridis')
 
-my_dpi = 72
-xpix = 1920/4.
-ypix = 1080/4.
+
+#to center the plot
 
 ohare_lon = -87.8369
 ohare_lat = 41.9773
+
+def define_args():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-f", "--fps",        type=int, help="frames per second [30]")
+	parser.add_argument("-d", "--duration",   type=int, help="number of seconds [30]")
+	parser.add_argument("-r", "--dpi",        type=int, help="dpi resolution [72]")
+	parser.add_argument("-x", "--xpix",       type=int, help="number of x pixels [1920]")
+	parser.add_argument("-y", "--ypix",       type=int, help="number of y pixels [1080]")
+	parser.add_argument("-c", "--cmap",       type=str, help="color map [viridis]")
+	parser.add_argument("-p", "--file",       type=str, help="input file [flightData.csv]")   
+	parser.add_argument("-i", "--istart",     type=int, help="starting frame [0]")   
+	parser.add_argument("-a", "--alpha",      type=float, help="alpha [0.1]")  
+	parser.add_argument("-b", "--tfac",       type=float, help="time factor for length of line [100]") 
+	parser.add_argument("-l", "--linelen",    type=int, help="default line length [2000]")
+	parser.add_argument("-w", "--linewidth",  type=int, help="default line width [1]")	
+	parser.add_argument("-g", "--color1",     type=float, help="colormap location of ORD [0.9]")	
+	parser.add_argument("-j", "--color2",     type=float, help="colormap location of other airports [0.5]")	
+	parser.add_argument("-m", "--ms",        type=int, help="marker size [5]")
+
+
+	#https://docs.python.org/2/howto/argparse.html
+	args = parser.parse_args()
+	
+	#apply the defaults
+	if (args.fps is None):
+		args.fps = 30
+	if (args.duration is None):
+		args.duration = 30
+	if (args.dpi is None):
+		args.dpi = 72
+	if (args.xpix is None):
+		args.xpix = 1920
+	if (args.ypix is None):
+		args.ypix = 1080
+	if (args.cmap is None):
+		args.cmap = 'viridis'
+	if (args.file is None):
+		args.file = 'flightData.csv'
+	if (args.istart is None):
+		args.istart = 0
+	if (args.alpha is None):
+		args.alpha = 0.1
+	if (args.tfac is None):
+		args.tfac = 100.
+	if (args.linelen is None):
+		args.linelen = 1000
+	if (args.linewidth is None):
+		args.linewidth = 1
+	if (args.color1 is None):
+		args.color1 = 0.9
+	if (args.color2 is None):
+		args.color2 = 0.5
+	if (args.ms is None):
+		args.ms = 5
+
+	#to print out the options that were selected (probably some way to use this to quickly assign args)
+	opts = vars(args)
+	options = { k : opts[k] for k in opts if opts[k] is not None }
+	print(options)
+
+	return args
+
 
 def isFloat(x):
 	try:
@@ -34,21 +100,17 @@ class LowerThresholdPlateCarree(ccrs.PlateCarree):
 	def threshold(self):
 		return 0.15
 	
-def drawLineSegment(ax, lon1, lat1, lon2, lat2, dt, llen=20, alpha=0.8, linewidth=5, color=cmap(0.9), ms=20):
-	if (dt < llen):
-		llen = dt
-		dt = 0
-
+def drawLineSegment(ax, lon1, lat1, lon2, lat2, linelen, offsetlen, alpha, linewidth, color):
 		
-	#print(dt, llen, ms, lon, lat, ohare_lon, ohare_lat)
+	#print(lon1, lat1, lon2, lat2, color, alpha)
 	
 	x = ax.plot([lon1, lon2], [lat1, lat2],
 		color=color, alpha=alpha, transform=ccrs.Geodetic(),
-		linewidth=linewidth, dashes=[0, dt, llen, 2000], dash_capstyle='round')
+		linewidth=linewidth, dashes=[0, offsetlen, linelen, 2000], dash_capstyle='round')
 	#print(len(x), x[0].get_path().__dict__)
 
 		
-def drawMap(df, t, i, nframes, tfac = 100., llen=20, alpha=0.8, linewidth=5, color1=cmap(0.9), color2=cmap(0.5), ms=20, mstlen=5):
+def drawMap(df, t, shrinki, nframes, color1, color2, xpix, ypix, dpi, tfac, linelen, alpha, linewidth, ms,fname=None):
 
 	#create the map projection
 	proj = LowerThresholdPlateCarree(central_longitude=ohare_lon)
@@ -56,15 +118,15 @@ def drawMap(df, t, i, nframes, tfac = 100., llen=20, alpha=0.8, linewidth=5, col
 	#proj = ccrs.Mollweide(central_longitude=ohare_lon)
 	#proj = ccrs.Robinson(central_longitude=ohare_lon)
 	data_crs = LowerThresholdPlateCarree()
-	f = plt.figure(figsize=(xpix/my_dpi, ypix/my_dpi), dpi=my_dpi, frameon=False) 
+	f = plt.figure(figsize=(xpix/dpi, ypix/dpi), dpi=dpi, frameon=False) 
 	ax = f.add_axes([0, 0, 1, 1], projection = proj)
 	ax.axis('off')
 	ax.outline_patch.set_visible(False)
 	#ax.set_global()
-	shrink = float(i)/float(nframes)*40.
+	shrink = float(shrinki)/float(nframes)*40.
 	extent = [-180 + shrink , 180 - shrink, -90 + shrink/2., 90 - shrink/2.]
 	#extent = [-180, 180, -90, 90]
-	print(shrink, extent)
+	#print(shrink, extent)
 	ax.set_extent(extent, crs=proj)
 
 	ax.background_patch.set_facecolor('k')
@@ -77,70 +139,78 @@ def drawMap(df, t, i, nframes, tfac = 100., llen=20, alpha=0.8, linewidth=5, col
 	# ax.add_feature(cartopy.feature.COASTLINE, linewidth=1, edgecolor=cmap(0.3))
 	# ax.add_feature(cartopy.feature.BORDERS, linewidth=1, edgecolor=cmap(0.3))
 
-
+	ORDafac = 4.
+	ORDlfac = 2.
+	ORDmfac = 2.
 	for index, row in df.iterrows():
 		c = color2
-		if (row['arrival_airport'] == 'KORD'):
+		lw = linewidth
+		a = alpha
+		if (row['destination_airport'] == 'ORD'):
 			c = color1
-		dt = (t - float(row['departure_time']))*tfac
-		drawLineSegment(ax, row['departure_longitude'], row['departure_latitude'], 
-						row['arrival_longitude'], row['arrival_latitude'], dt,  
-						llen=llen, alpha=alpha, linewidth=linewidth, color=c, ms=ms)
+			lw = linewidth*ORDlfac
+			a = alpha*ORDafac
+		llen = (t - float(row['source_departure_time']))*tfac
+		# drawLineSegment(ax, row['source_longitude'], row['source_latitude'], 
+		# 				row['destination_longitude'], row['destination_latitude'], offsetlen=0,  
+		# 				linelen=llen, alpha=a, linewidth=lw, color=c)
 
-	departing = df.loc[((t - df['departure_time'].astype(float))*tfac < mstlen) & (df['arrival_airport'] != 'KORD')]
-	departingORD = df.loc[((t - df['departure_time'].astype(float))*tfac < mstlen) & (df['arrival_airport'] == 'KORD')]
-	#marker at departure
-	if (len(departing) > 0):
-		ax.plot(departing['departure_longitude'].values, departing['departure_latitude'].values, 
+		drawLineSegment(ax, row['source_longitude'], row['source_latitude'], 
+						row['destination_longitude'], row['destination_latitude'], offsetlen=0,  
+						linelen=llen, alpha=a, linewidth=lw, color=c)
+
+	destinationOther = df.loc[(df['source_departure_time'].astype(float)<= t) & (df['destination_airport'] != 'ORD')]
+	destinationORD = df.loc[(df['source_departure_time'].astype(float)<= t) & (df['destination_airport'] == 'ORD')]
+	#marker at source
+	if (len(destinationOther) > 0):
+		ax.plot(destinationOther['source_longitude'].values, destinationOther['source_latitude'].values, 
 				color=color2, alpha=alpha, transform=ccrs.Geodetic(),
 				marker='o', ms=ms, mfc=color2, mew=0, linewidth=0)
-	if (len(departingORD) > 0):
-		ax.plot(departingORD['departure_longitude'].values, departingORD['departure_latitude'].values, 
-				color=color1, alpha=alpha, transform=ccrs.Geodetic(),
-				marker='o', ms=ms, mfc=color1, mew=0, linewidth=0)	
+	if (len(destinationORD) > 0):
+		ax.plot(destinationORD['source_longitude'].values, destinationORD['source_latitude'].values, 
+				color=color1, alpha=ORDafac*alpha, transform=ccrs.Geodetic(),
+				marker='o', ms=ORDmfac*ms, mfc=color1, mew=0, linewidth=0)	
 
 	#https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.lines.Line2D.html#matplotlib.lines.Line2D.get_path
-	#want marker at arrival?
+	#want marker at destination?
 #     if (dt < llen):
 #         ax.plot([ohare_lon], [ohare_lat], marker='o', ms=ms, mfc=color, mew=0)
 #         if (index > 1000):
 #             break
 
-	f.savefig('plots/flights_'+str(i).zfill(3)+'.png', format='png', bbox_inches='tight', dpi=my_dpi)
+	if (fname is None):
+		fname = 'plots/flights_'+str(i).zfill(3)+'.png'
+	f.savefig(fname, format='png', bbox_inches='tight', dpi=dpi)
 	plt.close()
 
 
 if __name__ == "__main__":
-	#read in all the files
-	flights_df = None
-	for f in os.listdir('flightData'):
-		df = pd.read_csv(os.path.join('flightData',f))
-		if (flights_df is None):
-			flights_df = df
-		else:
-			flights_df = pd.concat([flights_df, df])
 
-	#print(flights_df)
+	args = define_args()
 
-	#remove any that didn't get converted correctly
-	mask = np.array([isFloat(x) for x in flights_df["departure_time"].values])
-	flights_df = flights_df[mask]
-	mask = np.array([isFloat(x) for x in flights_df["arrival_time"].values])
-	flights_df = flights_df[mask]
+	cmap = matplotlib.cm.get_cmap(args.cmap)
+	nframes = args.duration*args.fps
 
-	#flights_df = flights_df[0:1000]
+	flights_df = pd.read_csv(args.file)
 
-	tmin = float(min(flights_df['departure_time'].astype(float).values))
-	tmax = float(max(flights_df['arrival_time'].astype(float).values))
+	#flights_df = flights_df[0:10000]
+
+	tmin = float(min(flights_df['source_departure_time'].astype(float).values))
+	tmax = float(max(flights_df['source_departure_time'].astype(float).values))
 	print(tmin, tmax)
 
-	iStart = 0
-	nframes = 500
+	# drawMap(flights_df, tmax, 0, 1, cmap(args.color1), cmap(args.color2), args.xpix, args.ypix, args.dpi, args.tfac, args.linelen, args.alpha, args.linewidth, args.ms, fname='testPlot.png')
+
 	for i, t in enumerate(np.linspace(tmin, tmax, nframes)):
-		if (i >= iStart):
-			inTime = flights_df.loc[flights_df["departure_time"].astype(float) <= t]
-			print(i, t, len(inTime["departure_time"]))
-			drawMap(inTime, t, i, nframes, llen=2000, alpha=0.5, linewidth=1, ms=5, mstlen=20)
+		if (i >= args.istart):
+			inTime = flights_df.loc[flights_df["source_departure_time"].astype(float) <= t]
+			print("plotting", i, t, len(inTime["source_departure_time"]))
+			try:
+				sys.stdout.flush()
+			except:
+				print("can't flush stdout")
+			drawMap(inTime, t, i, nframes, cmap(args.color1), cmap(args.color2), args.xpix, args.ypix, args.dpi, args.tfac, args.linelen, args.alpha, args.linewidth, args.ms)
+
 
 
 	print('done.')
@@ -150,3 +220,4 @@ if __name__ == "__main__":
 # git filter-branch --force --index-filter \
 #   "git rm --cached --ignore-unmatch flightPaths.mp4" \
 #   --prune-empty --tag-name-filter cat -- --all
+
---
+++ runPlotter.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sys
+import time
+import subprocess
+import os
+
+checkTime = 1 #seconds between checks of stdout
+istart = 0
+
+def waitproc(proc, startval):
+	while proc.poll() is None:
+		time.sleep(checkTime) #check this every x seconds
+		for line in iter(proc.stdout.readline, b''):
+			x = line.decode("utf-8").split()
+
+			if (x[0].strip() == 'plotting'):
+				startval = x[1].strip()
+
+			if (x[0].strip() == 'done.'):
+				return -1
+
+			print(">>> " + line.decode("utf-8"), startval)
+	return startval
+
+def followProcess(istart=0):
+	proc = subprocess.Popen(['./plotPaths.py', '-i', str(istart)], stdout=subprocess.PIPE)
+
+	startval = waitproc(proc, istart)
+
+	print('process finished...')
+
+	if (startval > 0):
+		istart = startval
+		print('restarting with istart = ',istart)
+		followProcess(istart)
+
+	else:
+		print('finished.')
+
+
+if __name__ == "__main__":
+
+	followProcess();
+	
